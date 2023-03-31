463a2a52dcf44c34626d5d9642dd94805102dba4
Bhoomik Shetty <76581406+bhoomikshetty@users.noreply.github.com>
2023-04-01 01:14:20 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.post('/submit', (req, res) => {
+  const formData = req.body;
+  console.log(formData);
+  res.send('Form submitted successfully!');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
